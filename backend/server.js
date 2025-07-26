b6e483f17b1abd72dec7764bfe6fa444caec9543
Ankit-0018 <as1777766@gmail.com>
2025-07-26 16:23:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use('/api/users' , verifyJwt ,userRouter)
 app.use('/api/items' , verifyJwt , itemRouter)
 app.use('/api/notifications' , verifyJwt , notificationRouter)
 app.use('/api' , verifyJwt, deleteRouter)
-app.get("*", (req, res) => {
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
